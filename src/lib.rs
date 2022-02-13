4f90d3e46305a6cdde761129e68d8b6481a5bb97
megumish <megumish@exploitouri.st>
2022-02-13 23:51:53 +0900

    feat(*): change parse method

--- src/lib.rs
+++ src/lib.rs
@@ -1,113 +1,170 @@
-mod as_c_struct;
-mod parser;
-
-pub struct ELFHeader<'a, Class> {
-    pub endian: Endian,
-    pub version: Version<'a>,
-    pub os_abi: OSABI<'a>,
-    pub file_type: FileType<'a>,
-    pub machine: MachineType<'a>,
-    pub entry: Address<'a>,
-    pub program_header_offset: &'a Class,
-    pub section_header_offset: &'a Class,
-    pub flags: &'a u32,
-    pub elf_header_size: &'a u32,
-    pub program_header_entry_size: &'a u32,
-    pub program_header_number: &'a u32,
-    pub section_header_entry_size: &'a u32,
-    pub section_header_number: &'a u32,
-    pub section_header_string_table_index: &'a u32,
-}
-
-impl<Class> ELFHeader<'_, Class> {
-    fn from_c_struct(h: as_c_struct::ELFHeader<Class>) -> Self {
-        Self {
-            endian: if h.e_ident == 1 {
-                Endian::Little
-            } else {
-                Endian::Big
-            },
-            version: if h.e_version == 0 {
-                Version::None
-            } else if h.e_version == 1 {
-                Version::Current
-            } else {
-                Version::Unknown(&h.e_ident.version)
-            },
-            
-        }
-    }
+#[derive(Debug)]
+#[repr(C)]
+struct ELFHeader<Class>
+where
+    Class: SwapBytes + Copy,
+{
+    pub e_ident: ELFIdentifier,
+    pub e_type: FileType,
+    pub e_machine: MachineType,
+    pub e_version: u32,
+    pub e_entry: Class,
+    pub e_phoff: Class,
+    pub e_shoff: Class,
+    pub e_flags: u32,
+    pub e_ehsize: u16,
+    pub e_phentsize: u16,
+    pub e_phnum: u16,
+    pub e_shentsize: u16,
+    pub e_shnum: u16,
+    pub e_shstrndx: u16,
 }
 
 #[derive(Debug, PartialEq, Clone, Copy)]
-#[non_exhaustive]
+#[repr(u8)]
 pub enum Endian {
+    None,
     Little,
     Big,
 }
 
-#[derive(Debug, PartialEq)]
-#[non_exhaustive]
-pub enum Version<'a> {
+#[derive(Debug, PartialEq, Clone, Copy)]
+#[repr(u8)]
+pub enum Version {
     None,
     Current,
-    Unknown(&'a u8),
 }
 
-#[derive(Debug, PartialEq)]
-#[non_exhaustive]
-pub enum OSABI<'a> {
-    None,
+#[derive(Debug, PartialEq, Clone, Copy)]
+#[repr(u8)]
+pub enum OSABI {
     SystemV,
     HPUX,
     NetBSD,
     GNUOrLinux,
-    Solaris,
-    AIX,
-    SGIIrix,
-    FreeBSD,
-    TRU64,
-    NovellModesto,
-    OpenBSD,
-    ArmEABI,
-    Arm,
-    Standalone,
-    Unknown(&'a u8),
+    Solaris = 6,
+    AIX = 7,
+    SGIIrix = 8,
+    FreeBSD = 9,
+    TRU64 = 10,
+    NovellModesto = 11,
+    OpenBSD = 12,
+    ArmEABI = 64,
+    Arm = 97,
+    Standalone = 255,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 #[non_exhaustive]
-pub enum MachineType<'a> {
-    AMD64,
-    Unknown(&'a u16),
+#[repr(u16)]
+pub enum MachineType {
+    AMD64 = 62,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 #[non_exhaustive]
-pub enum FileType<'a> {
+#[repr(u16)]
+pub enum FileType {
     None,
     Executable,
     Relocatble,
     Core,
     DynamicLibrary,
     Number,
-    Unknown(&'a u16),
+    Unknown,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 #[non_exhaustive]
-pub enum ELFClass<'a> {
+#[repr(u8)]
+pub enum ELFClass {
     None,
     ELF32,
     ELF64,
-    Unknown(&'a u8),
 }
 
-// TODO: display this as hex value
-#[derive(Debug, PartialEq)]
-pub struct Address<'a>(&'a u64);
+impl<Class> ELFHeader<Class>
+where
+    Class: SwapBytes + Copy,
+{
+    fn swap_bytes(&self) -> Self {
+        Self {
+            e_ident: self.e_ident,
+            e_type: self.e_type.swap_bytes(),
+            e_machine: self.e_machine.swap_bytes(),
+            e_version: self.e_version.swap_bytes(),
+            e_entry: self.e_entry.swap_bytes(),
+            e_phoff: self.e_phoff.swap_bytes(),
+            e_shoff: self.e_shoff.swap_bytes(),
+            e_flags: self.e_flags.swap_bytes(),
+            e_ehsize: self.e_ehsize.swap_bytes(),
+            e_phentsize: self.e_phentsize.swap_bytes(),
+            e_phnum: self.e_phnum.swap_bytes(),
+            e_shentsize: self.e_shentsize.swap_bytes(),
+            e_shnum: self.e_shnum.swap_bytes(),
+            e_shstrndx: self.e_shstrndx.swap_bytes(),
+        }
+    }
+}
 
-pub enum Error<'a> {
-    InvalidMagic(&'a [u8]),
-    InvalidFile,
+#[derive(Debug, Clone, Copy)]
+#[repr(C)]
+struct ELFIdentifier {
+    pub magic: [u8; 4],
+    pub class: ELFClass,
+    pub data: Endian,
+    pub version: Version,
+    pub os_abi: OSABI,
+    pub os_abi_version: u8,
+    pub padding: [u8; 7],
+}
+
+pub trait SwapBytes {
+    fn swap_bytes(self) -> Self;
+}
+
+impl SwapBytes for u32 {
+    fn swap_bytes(self) -> u32 {
+        self.swap_bytes()
+    }
+}
+
+impl SwapBytes for u64 {
+    fn swap_bytes(self) -> u64 {
+        self.swap_bytes()
+    }
+}
+
+impl SwapBytes for FileType {
+    fn swap_bytes(self) -> FileType {
+        unsafe {
+            *(&(std::mem::transmute::<FileType, u16>(self).swap_bytes()) as *const u16
+                as *const FileType)
+        }
+    }
+}
+
+impl SwapBytes for MachineType {
+    fn swap_bytes(self) -> MachineType {
+        unsafe {
+            *(&(std::mem::transmute::<MachineType, u16>(self).swap_bytes()) as *const u16
+                as *const MachineType)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::mem::size_of;
+
+    use crate::ELFHeader;
+
+    #[test]
+    fn it_works() {
+        let bytes = include_bytes!("main");
+        let header = unsafe {
+            &*(&bytes[0..size_of::<ELFHeader<u64>>()] as *const [u8] as *const ELFHeader<u64>)
+        };
+        println!("{header:#x?}")
+    }
 }
